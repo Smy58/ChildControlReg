03d6acf4ed59f6fba7db5da07d668ce32d0fa822
Ilshat Osmanov <190103274@stu.sdu.edu.kz>
2023-10-09 11:32:22 +0600

    fix config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/my-project/'
-    : '/'
-}
+module.exports = defineConfig({
+  transpileDependencies: true
+})
